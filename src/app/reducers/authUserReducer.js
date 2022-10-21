1fbf858b8d9fd8905d700eb426508ec39cdf1845
Tyrhen <williamstm6@vcu.edu>
2022-10-20 22:48:48 -0400

    Create polls reducer + Keep myself logged in

--- src/app/reducers/authUserReducer.js
+++ src/app/reducers/authUserReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: "",
+  value: "sarahedo",
 };
 
 export const authUserSlice = createSlice({
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./reducers/apiReducer";
 import { authUserSlice } from "./reducers/authUserReducer";
+import { pollsSlice } from "./reducers/pollReducer";
 
 export const store = configureStore({
   reducer: {
     authUser: authUserSlice.reducer,
+    polls: pollsSlice.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
