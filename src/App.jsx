3933307c5f1aa587a6790be7eac71992f7a18f29
Tyrhen <williamstm6@vcu.edu>
2022-10-20 22:48:24 -0400

    Make nice animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import DisplayModeButton from "./components/DisplayMode";
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import LogoutButton from "./components/Logout";
+import { fetchPolls } from "./app/reducers/pollReducer";
+import { useDispatch } from "react-redux";
 
 export default function App() {
+  const dispatch = useDispatch();
+  dispatch(fetchPolls());
   const [colorScheme, setColorScheme] = useState("dark");
   const toggleColorScheme = (value) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
