e6e7892f57cce6320541495d44cea8fb685e8943
Tyrhen <williamstm6@vcu.edu>
2022-10-20 22:49:06 -0400

    Display polls in sorted order

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,26 @@ import { useSelector } from "react-redux";
 
 export default function Dashboard() {
   const authUser = useSelector((state) => state.authUser.value);
+  const polls = useSelector((state) => state.polls.value);
+  const pollsArray = Object.keys(polls)
+    .map((id) => polls[id])
+    .sort((a, b) => b.timestamp - a.timestamp);
+
+  console.log(pollsArray);
 
   return (
-    <>
-      <h2> Welcome {authUser}!</h2>
-    </>
+    <div>
+      <h1>Dashboard</h1>
+      <h2>{authUser}</h2>
+      <ul>
+        {pollsArray.map((poll) => (
+          <div key={poll.id}>
+            <li>{poll.author}</li>
+            <li>{poll.optionOne.text}</li>
+            <li>{poll.optionTwo.text}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
   );
 }
