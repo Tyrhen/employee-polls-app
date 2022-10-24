d85b5371588d1c31d2149ec3d4ef53b40b98b5c3
Tyrhen <williamstm6@vcu.edu>
2022-10-23 21:06:53 -0400

    Persist some state sometimes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { authUserSlice } from "./reducers/authUserReducer";
 import { pollsSlice } from "./reducers/pollReducer";
 import { usersSlice } from "./reducers/userReducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  authUser: authUserSlice.reducer,
+  polls: pollsSlice.reducer,
+  users: usersSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    authUser: authUserSlice.reducer,
-    polls: pollsSlice.reducer,
-    users: usersSlice.reducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
